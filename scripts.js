3d6f3cc046732488f49ec7dc6dce3182aaa9872f
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2024-08-09 00:10:50 -0300

    update:  atualiza lista a cada dez segundos

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 let nome = prompt("Digite seu nome:");
 
-
 // Função para esconder e mostrar o sidebar
 document.addEventListener("DOMContentLoaded", () => {
   const sidebar = document.querySelector(".sidebar");
@@ -20,26 +19,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Função para pedir o nome do usuário ao entrar na sala
 function solicitarNome() {
-
   const promessa = axios
     .post(
       "https://mock-api.driven.com.br/api/v6/uol/participants/df10e74f-e839-4a24-8fd0-08cc129c0608",
       { name: nome }
     )
     .then((a) => {
-        alert(`Bem vindo ${nome}`)
-        receberMensagens(); // carrega as mensagens ao iniciar conexão
-        setInterval(receberMensagens, 3000); // atualiza as mensagens a cada 3seg
-        setInterval(manterPresenca, 5000); // atualiza a presença a cada seg, para não "quicar" a pessoa da sala
-        
+      alert(`Bem vindo ${nome}`);
+      receberMensagens(); // carrega as mensagens ao iniciar conexão
+      setInterval(receberMensagens, 3000); // atualiza as mensagens a cada 3seg
+      setInterval(manterPresenca, 5000); // atualiza a presença a cada seg, para não "quicar" a pessoa da sala
     })
     .catch((error) => {
       alert("Nome já está em uso");
-      solicitarNome();  // solicita o nome da pessoa novamente caso já esteja em uso
+      solicitarNome(); // solicita o nome da pessoa novamente caso já esteja em uso
     });
 }
 
-// Função para fazer as mensagens 
+// Função para fazer as mensagens
 
 function enviarMensagem() {
   const text = document.querySelector(".text").value;
@@ -54,43 +51,60 @@ function enviarMensagem() {
     return;
   }
 
-  axios.post("https://mock-api.driven.com.br/api/v6/uol/messages/df10e74f-e839-4a24-8fd0-08cc129c0608", {
-    from: from,
-    to: to,
-    text: text,
-    type: type
-  })
-  .then(response => {
-    console.log("Mensagem enviada com sucesso!", response.data);
-    document.querySelector(".text").value = ""; 
-    receberMensagens(); 
-  })
-  .catch(error => {
-    console.error("Erro ao enviar mensagem:", error.response ? error.response.data : error.message);
-  });
+  axios
+    .post(
+      "https://mock-api.driven.com.br/api/v6/uol/messages/df10e74f-e839-4a24-8fd0-08cc129c0608",
+      {
+        from: from,
+        to: to,
+        text: text,
+        type: type,
+      }
+    )
+    .then((response) => {
+      console.log("Mensagem enviada com sucesso!", response.data);
+      document.querySelector(".text").value = "";
+      receberMensagens();
+    })
+    .catch((error) => {
+      console.error(
+        "Erro ao enviar mensagem:",
+        error.response ? error.response.data : error.message
+      );
+    });
 }
 
-// Função para receber as mensagens 
+// Função para receber as mensagens
 
-function receberMensagens(){
-  axios.get("https://mock-api.driven.com.br/api/v6/uol/messages/df10e74f-e839-4a24-8fd0-08cc129c0608")
-  .then(response => {
-    console.log("Dados da resposta", response.data);
-    mostrarMensagens(response.data);
-  })
-  .catch(error => {
-    console.error("Erro ao receber mensagem:", error.response ? error.response.data : error.message);
-  });
+function receberMensagens() {
+  axios
+    .get(
+      "https://mock-api.driven.com.br/api/v6/uol/messages/df10e74f-e839-4a24-8fd0-08cc129c0608"
+    )
+    .then((response) => {
+      console.log("Dados da resposta", response.data);
+      mostrarMensagens(response.data);
+    })
+    .catch((error) => {
+      console.error(
+        "Erro ao receber mensagem:",
+        error.response ? error.response.data : error.message
+      );
+    });
 }
 
-
-// Função mostrar mensagens 
+// Função mostrar mensagens
 function mostrarMensagens(mensagens) {
   const telaMensagens = document.querySelector(".telaMensagens");
-  telaMensagens.innerHTML = ""; 
+  telaMensagens.innerHTML = "";
 
-  mensagens.forEach(mensagem => {
-    if (mensagem.type === "status" || (mensagem.type === "private_message" && (mensagem.from === nome || mensagem.to === nome)) || mensagem.type === "message") {
+  mensagens.forEach((mensagem) => {
+    if (
+      mensagem.type === "status" ||
+      (mensagem.type === "private_message" &&
+        (mensagem.from === nome || mensagem.to === nome)) ||
+      mensagem.type === "message"
+    ) {
       const mensagemElement = document.createElement("div");
       mensagemElement.classList.add("mensagem");
 
@@ -115,14 +129,21 @@ function mostrarMensagens(mensagens) {
   telaMensagens.scrollTop = telaMensagens.scrollHeight;
 }
 
-// Função para manter presença 
+// Função para manter presença
 function manterPresenca() {
-  axios.post("https://mock-api.driven.com.br/api/v6/uol/status/df10e74f-e839-4a24-8fd0-08cc129c0608", { name: nome })
-    .then(response => {
+  axios
+    .post(
+      "https://mock-api.driven.com.br/api/v6/uol/status/df10e74f-e839-4a24-8fd0-08cc129c0608",
+      { name: nome }
+    )
+    .then((response) => {
       console.log("Presença confirmada:", response.data);
     })
-    .catch(error => {
-      console.error("Erro ao manter presença:", error.response ? error.response.data : error.message);
+    .catch((error) => {
+      console.error(
+        "Erro ao manter presença:",
+        error.response ? error.response.data : error.message
+      );
       if (error.response && error.response.status === 400) {
         alert("Você não está mais na sala. Recarregando a página...");
         location.reload();
@@ -130,18 +151,23 @@ function manterPresenca() {
     });
 }
 
+// Função obter a lista de participantes
 
-// Função obter a lista de participantes 
-
-function obterParticipantes(){
-  axios.get("https://mock-api.driven.com.br/api/v6/uol/participants/df10e74f-e839-4a24-8fd0-08cc129c0608")
-  .then( response => {
-    console.log("cheguei aqui", response.data);
-    atualizarListaParticipantes(response.data)
-  })
-  .catch(error => {
-    console.error("Erro ao obter participantes:", error.response ? error.response.data : error.message);
-  })
+function obterParticipantes() {
+  axios
+    .get(
+      "https://mock-api.driven.com.br/api/v6/uol/participants/df10e74f-e839-4a24-8fd0-08cc129c0608"
+    )
+    .then((response) => {
+      console.log("cheguei aqui", response.data);
+      atualizarListaParticipantes(response.data);
+    })
+    .catch((error) => {
+      console.error(
+        "Erro ao obter participantes:",
+        error.response ? error.response.data : error.message
+      );
+    });
 }
 
 /* for(let i=0; participantes.length; i++){
@@ -149,29 +175,26 @@ function obterParticipantes(){
 
 }*/
 
-// função para atualizar a lista no sidebar 
+// função para atualizar a lista no sidebar
 
 function atualizarListaParticipantes(participantes) {
-  const listaParticipantes = document.querySelector('.lista-participantes');
-  listaParticipantes.innerHTML = '';
+  const listaParticipantes = document.querySelector(".lista-participantes");
+  const itemFixo = listaParticipantes.querySelector(".item-fixo");
+  const itens = Array.from(
+    listaParticipantes.querySelectorAll("li:not(.item-fixo)")
+  );
+  itens.forEach((item) => item.remove());
 
+  participantes.forEach((participante) => {
+    console.log("Participante:", participante);
 
-  participantes.forEach(participante => {
-    console.log("Participante:", participante); 
+    const item = document.createElement("li");
 
+    const icon = document.createElement("ion-icon");
+    icon.setAttribute("name", "person");
 
-    const item = document.createElement('li');
-
-    const icon = document.createElement('ion-icon');
-    icon.setAttribute('name', 'person');
-
-  
-    icon.style.fontSize = '24px';
-    icon.style.marginRight = '10px';
-
-  
-    const name = document.createElement('span');
-    name.textContent = participante.name || 'Nome não disponível';
+    const name = document.createElement("span");
+    name.textContent = participante.name || "Nome não disponível";
     console.log("Nome do participante:", name.textContent);
 
     item.appendChild(icon);
@@ -182,4 +205,5 @@ function atualizarListaParticipantes(participantes) {
 }
 
 obterParticipantes();
+setInterval(obterParticipantes, 10000);
 solicitarNome();
